233c8560d9c2418ab25b4b21140922e465ca2d9a
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-03-24 22:34:21 +0900

    advised

---
+++ ex3_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ex3_4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int cnt = 0;
+        while(true){
+            int compare = (N / K) * K;
+            cnt += N - compare;
+            N = compare;
+            if(N < K){
+                break;
+            }
+            cnt++;
+            N /= K;
+        }
+
+        cnt += N - 1;
+        System.out.println(cnt);
+    }
+}
