bcf7dc42246de8b55a4996db6649647a931f5352
kgw753 <49424965+kgw753@users.noreply.github.com>
2021-04-14 21:21:02 +0900

    solved

--- ex11_5.java
+++ ex11_5.java
@@ -1,36 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Ball{
-    public int index;
-    public int weight;
-
-    public Ball(int index, int weight){
-        this.index = index;
-        this.weight = weight;
-    }
-}
-
 public class ex11_5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        ArrayList<Ball> arr = new ArrayList<>();
+        int[] arr = new int[m + 1];
 
         for(int i = 1; i <= n; i++){
             int w = sc.nextInt();
-            arr.add(new Ball(i, w));
+            arr[w]++;
         }
         
-        int c = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(arr.get(i).weight == arr.get(j).weight) continue;
-                c++;
-            }
+        int res = 0;
+        for(int i = 1; i < m; i++){
+            n -= arr[i];
+            res += arr[i] * n;
         }
 
-        System.out.println(c);
+        System.out.println(res);
     }
 }
